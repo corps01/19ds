f193762352f58dab17093d7dc378d1a507fe3825
corps01 <50376088+corps01@users.noreply.github.com>
2019-09-08 20:29:03 -0500

    Add files via upload

---
+++ repeated().js
@@ -0,0 +1,24 @@
+const repeated = arr => {
+let repeated = []
+let temp
+for (let i = 0; i < arr.length; i++) {
+    for (j = i+1; j < arr.length; j++) {
+      if(arr[i] === arr[j])
+        repeated.push(arr[i])
+        } 
+  
+    }
+        if(Array.from(new Set(repeated)).length <= 0){
+            return false
+        }   else {
+            return true
+    }
+}
+
+//REPETIDOS
+console.log(repeated([7,7,7,7,7,2,2,32,17,94,7,14543654,-1,2]))
+
+//SIN REPETIDOS
+console.log(repeated([7,2]))
+
+//ARRAY.FROM CONSULTADO DE https://gomakethings.com/removing-duplicates-from-an-array-with-vanilla-javascript/ 
