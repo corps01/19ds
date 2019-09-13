459400dd222b7d7676778311a577c242367107d3
corps01 <50376088+corps01@users.noreply.github.com>
2019-09-13 10:55:10 -0500

    Update and rename logbxBase.js to base_b.js

--- base_b.js
+++ base_b.js
@@ -3,7 +3,7 @@ let result
 const log7 = (x,y) => Math.log10(x)/Math.log10(y)
 const base = prompt("Ingresa la base")
 
-for(i=0;i<=10; i++){
+for(i=1;i<=10; i++){
     result = log7(i,base)
     console.log("log base " + base + "= (" + i + ") = " +result)
 }
