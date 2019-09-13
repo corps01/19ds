e57f244986bb6d87fc355091048902e72ef88028
corps01 <50376088+corps01@users.noreply.github.com>
2019-09-13 10:48:51 -0500

    Update and rename logbx7.js to base7.js

--- base7.js
+++ base7.js
@@ -2,7 +2,7 @@ let result
 
 const log7 = x => Math.log10(x)/Math.log10(7)
 
-for(i=0;i<=10; i++){
+for(i=1;i<=10; i++){
     result = log7(i)
     console.log("log base 7: ("+ i +") = " +result)
 }
